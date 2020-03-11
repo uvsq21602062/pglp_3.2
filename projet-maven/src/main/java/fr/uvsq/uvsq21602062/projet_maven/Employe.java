81a96e9234621340f2638353341db02c849a5d25
Jean Destribois <jean.destribois@gmail.com>
2020-03-11 11:04:21 +0100

    Premier commit d'initialisation du dépot avec projet maven

---
+++ projet-maven/src/main/java/fr/uvsq/uvsq21602062/projet_maven/Employe.java
@@ -0,0 +1,17 @@
+package fr.uvsq.uvsq21602062.projet_maven;
+
+public class Employe {
+	private float salaire;
+	
+	public Employe(float salaire) {
+		this.salaire = salaire;
+	}
+	
+	public float getSalaire() {
+		return this.salaire;
+	}
+	
+	public void setSalaire(float salaire) {
+		this.salaire = salaire;
+	}
+}
---
+++ projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/EmployeTest.java
@@ -0,0 +1,23 @@
+package fr.uvsq.uvsq21602062.projet_maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmployeTest {
+
+	@Test
+	public void testEmploye() {
+		
+	}
+	
+	@Test
+	public void testGetSalaire() {
+		
+	}
+	
+	@Test
+	public void testSetSalaire() {
+		
+	}
+}
