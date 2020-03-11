f159149d1360fe7b6b93a3b0f40fbc0160c970ad
Jean Destribois <jean.destribois@gmail.com>
2020-03-11 11:33:47 +0100

    Completion de la classe de test JUnit TestVendeur et creation des classes Entreprise et TestEntreprise

--- projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/VendeurTest.java
+++ projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/VendeurTest.java
@@ -7,8 +7,42 @@ import org.junit.Test;
 public class VendeurTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetSalaire() {
+		Vendeur D = new Vendeur(1000);
+		assertTrue(D.getSalaire() == 1000);
+	}
+	
+	@Test
+	public void testSetSalaire() {
+		Vendeur D = new Vendeur(1000);
+		D.setSalaire(500);
+		assertTrue(D.getSalaire() == 500);	
+	}
+	
+	@Test
+	public void testGetAnciennete() {
+		Vendeur D = new Vendeur(1000, 5);
+		assertTrue(D.getAnciennete() == 5);	
+	}
+	
+	@Test
+	public void testSetAnciennete() {
+		Vendeur D = new Vendeur(1000);
+		D.setAnciennete(5);
+		assertTrue(D.getAnciennete() == 5);	
+	}
+	
+	@Test
+	public void testGetCommission() {
+		Vendeur D = new Vendeur(1000, 5, 100);
+		assertTrue(D.getCommission() == 100);	
+	}
+	
+	@Test
+	public void testSetCommission() {
+		Vendeur D = new Vendeur(1000, 5);
+		D.setCommission(100);
+		assertTrue(D.getCommission() == 100);	
 	}
 
 }
